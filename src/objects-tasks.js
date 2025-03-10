9db6bb4aba736c2bf11a4d70767b9a0d0e9efd61
Navka <navka@list.ru>
2025-03-10 18:48:53 +0300

    feat: 4 problems solved

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -62,8 +62,14 @@ function mergeObjects(objects) {
  *    removeProperties({name: 'John', age: 30, city: 'New York'}, ['age']) => {name: 'John', city: 'New York'}
  *
  */
-function removeProperties(/* obj, keys */) {
-  throw new Error('Not implemented');
+function removeProperties(obj, keys) {
+  const objCopy = { ...obj };
+  keys.forEach((key) => {
+    if (key in objCopy) {
+      delete objCopy[key];
+    }
+  });
+  return objCopy;
 }
 
 /**
@@ -78,8 +84,8 @@ function removeProperties(/* obj, keys */) {
  *    compareObjects({a: 1, b: 2}, {a: 1, b: 2}) => true
  *    compareObjects({a: 1, b: 2}, {a: 1, b: 3}) => false
  */
-function compareObjects(/* obj1, obj2 */) {
-  throw new Error('Not implemented');
+function compareObjects(obj1, obj2) {
+  return JSON.stringify(obj1) === JSON.stringify(obj2);
 }
 
 /**
