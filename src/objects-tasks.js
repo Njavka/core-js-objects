88326d8d494ebd36dd0fdf8eca440f71cca55c36
Navka <navka@list.ru>
2025-03-10 18:41:07 +0300

    feat: 2 problems solved

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -17,8 +17,9 @@
  *    shallowCopy({a: 2, b: { a: [1, 2, 3]}}) => {a: 2, b: { a: [1, 2, 3]}}
  *    shallowCopy({}) => {}
  */
-function shallowCopy(/* obj */) {
-  throw new Error('Not implemented');
+function shallowCopy(obj) {
+  const copy = {};
+  return Object.assign(copy, obj);
 }
 
 /**
@@ -32,8 +33,20 @@ function shallowCopy(/* obj */) {
  *    mergeObjects([{a: 1, b: 2}, {b: 3, c: 5}]) => {a: 1, b: 5, c: 5}
  *    mergeObjects([]) => {}
  */
-function mergeObjects(/* objects */) {
-  throw new Error('Not implemented');
+function mergeObjects(objects) {
+  return Object.fromEntries(
+    objects.reduce((acc, obj) => {
+      Object.entries(obj).forEach(([key, value]) => {
+        if (acc.find(([k]) => k === key) && typeof value === 'number') {
+          const index = acc.findIndex(([k]) => k === key);
+          acc[index][1] += value;
+        } else {
+          acc.push([key, value]);
+        }
+      });
+      return acc;
+    }, [])
+  );
 }
 
 /**
